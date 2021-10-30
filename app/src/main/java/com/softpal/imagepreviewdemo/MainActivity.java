95842ef200ebf397fa43ddb69c65ae55252a903d
Softpal MacMini <softpal@Softpals-Mac-mini.local>
2021-10-30 16:20:14 +0530

    migrated to android X

--- app/src/main/java/com/softpal/imagepreviewdemo/MainActivity.java
+++ app/src/main/java/com/softpal/imagepreviewdemo/MainActivity.java
@@ -3,7 +3,7 @@ package com.softpal.imagepreviewdemo;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
